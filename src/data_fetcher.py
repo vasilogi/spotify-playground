31c08b93be2eb2fcf12f480e7bbcbe4c34573f83
Giannis Vasilopoulos <61663739+vasilogi@users.noreply.github.com>
2025-03-13 12:48:22 +0200

    add docstrings

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -43,43 +43,79 @@ class DataFetcher:
     """
 
     def __init__(self, spotify_client: spotipy.Spotify):
+        """
+        Initializes the DataFetcher with an authenticated Spotipy client.
+
+        Parameters:
+        ----------
+        spotify_client : spotipy.Spotify
+            An authenticated Spotipy client instance.
+        """
         self.sp: spotipy.Spotify = spotify_client
         self.pagination_limit: int = 50
 
 
     def calculate_total_albums(self) -> int:
-        
+        """
+        Calculates the total number of saved albums for the current user.
+
+        Returns:
+        -------
+        int
+            The total number of saved albums.
+        """
         limit: int = 1
-        
         try:
             results: Dict[str, Any] = self.sp.current_user_saved_albums(limit=limit)
         except spotipy.SpotifyException as e:
             raise SpotifyAPIError(f"Failed to fetch albums: {e}") from e
-        
         return results['total']
     
     def calculate_total_playlists(self) -> int:
-        
+        """
+        Calculates the total number of playlists for the current user.
+
+        Returns:
+        -------
+        int
+            The total number of playlists.
+        """
         limit: int = 1
-        
         try:
             results: Dict[str, Any] = self.sp.current_user_playlists(limit=limit)
         except spotipy.SpotifyException as e:
             raise SpotifyAPIError(f"Failed to fetch playlists: {e}") from e
-        
         return results['total']
     
     def calculate_total_tracks(self, playlist_id: str) -> int:
-        
+        """
+        Calculates the total number of tracks in a given playlist.
+
+        Parameters:
+        ----------
+        playlist_id : str
+            The ID of the playlist.
+
+        Returns:
+        -------
+        int
+            The total number of tracks in the playlist.
+        """
         try:
             playlist_info: Dict[str, Any] = self.sp.playlist(playlist_id)
             return playlist_info['tracks']['total']
         except spotipy.SpotifyException as e:
             raise SpotifyAPIError(f"Failed to fetch tracks from a playlist: {e}") from e
 
-    
     def fetch_all_albums(self, csv_filepath: str) -> None:
+        """
+        Fetches all saved albums for the current user and saves them to a CSV file.
 
+        Parameters:
+        ----------
+        csv_filepath : str
+            The file path where the CSV file will be saved.
+        """
         total_albums: int = self.calculate_total_albums()
         albums: List[Dict[str, Any]] = []
         offset: int = 0
@@ -87,7 +123,10 @@ class DataFetcher:
         with tqdm(total=total_albums, desc='Fetching all albums') as pbar:
             while True:
                 try:
-                    results: Dict[str, Any] = self.sp.current_user_saved_albums(limit=limit, offset=offset)
+                    results: Dict[str, Any] = self.sp.current_user_saved_albums(
+                        limit=limit,
+                        offset=offset
+                    )
                 except spotipy.SpotifyException as e:
                     raise SpotifyAPIError(f"Failed to fetch albums: {e}") from e
                 if not results['items']:
@@ -100,8 +139,7 @@ class DataFetcher:
                         'Release Date': album['release_date'],
                         'Popularity': album['popularity'],
                         'Image URL': album['images'][0]['url']
-                    })
-                
+                    })      
                 # Update offset and progress bar
                 offset += limit
                 pbar.update(len(results['items']))
@@ -115,18 +153,27 @@ class DataFetcher:
             raise FileWriteError("Unable to write to the CSV file: {e}") from e
         except Exception as e:
             raise UnexpectedError(f"An unexpected error occured while writing the CSV: {e}") from e
-        
+    
     def fetch_all_playlists(self, csv_filepath: str) -> None:
-        
+        """
+        Fetches all playlists for the current user and saves them to a CSV file.
+
+        Parameters:
+        ----------
+        csv_filepath : str
+            The file path where the CSV file will be saved.
+        """
         total_playlists: int = self.calculate_total_playlists()
         play_lists: List[Dict[str, Any]] = []
         offset: int = 0
-        limit: int = self.pagination_limit
-        
+        limit: int = self.pagination_limit   
         with tqdm(total=total_playlists, desc='Fetching all playlists') as pbar:
             # loop over all current user playlists
             while True:
-                current_user_playlists: Dict[str, Any] = self.sp.current_user_playlists(limit=limit, offset=offset)
+                current_user_playlists: Dict[str, Any] = self.sp.current_user_playlists(
+                    limit=limit,
+                    offset=offset
+                )
                 playlists: Dict[str, Any] = current_user_playlists['items']
                 # exit the loop if no playlists are found
                 if not playlists:
@@ -157,11 +204,20 @@ class DataFetcher:
                 ) from e
 
     def fetch_tracks_from_playlist(self, playlist_id: str, csv_filepath: str) -> None:
+        """
+        Fetches all tracks from a given playlist and saves them to a CSV file.
+
+        Parameters:
+        ----------
+        playlist_id : str
+            The ID of the playlist.
+        csv_filepath : str
+            The file path where the CSV file will be saved.
+        """
         total_tracks: int = self.calculate_total_tracks(playlist_id=playlist_id)
         tracks: List[Dict[str, Any]] = []
         offset: int = 0
         limit: int = self.pagination_limit
-        
         with tqdm(total=total_tracks, desc='Fetching all tracks from a playlist') as pbar:
             # loop over all current user playlists
             while True:
